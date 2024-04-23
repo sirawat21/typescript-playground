d73ff95564f26d1f2c0e957f4b5fab24d87b7c18
sirawat21 <sirawat21@gmail.com>
2024-04-23 13:54:24 +1000

    added action file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import * as core from "@actions/core";
-import { ErrorCallback } from "typescript";
 
 const main = () => {
     try {
         console.log("Hello World TS");
     } catch (error: ErrorCallback | any) {
         core.setFailed(error.message);
+        console.log(error.message);
     }
 };
 
